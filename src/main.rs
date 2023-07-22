718d1c3d5c44dac8b227a970ab71d623297c0990
meep334 <geraldd459@gmail.com>
2023-07-22 17:28:23 +0200

    more selectors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::collections::HashMap;
+use std::{any::Any, collections::HashMap, error::Error};
 
+use kuchiki::{
+    iter::{Descendants, Elements, Select},
+    traits::{ElementIterator, TendrilSink},
+    NodeRef,
+};
 use serde::{Deserialize, Serialize};
+use util::kuchiki_elements::ElementsTrait;
+pub mod util;
+
+type Element = Select<Elements<Descendants>>;
 
 fn main() {
     println!("Hello, world!");
@@ -59,11 +68,17 @@ struct Tester {
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
+#[serde(tag = "type")]
 enum TextType {
     #[default]
     Own,
-    All(),
-    Attribute(),
+    All {
+        join_str: String,
+    },
+    Attribute {
+        join_str: Option<String>,
+        attributes: Vec<String>,
+    },
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
@@ -71,6 +86,29 @@ struct QueryMatcher {
     selector: String,
     text_type: TextType,
     clean_with_regex_1: Option<String>,
+}
+
+impl QueryMatcher {
+    fn select<T: ElementIterator>(&self, node: T) -> Select<T> {
+        node.select(self.selector.as_str()).expect("Invalid Regex")
+    }
+
+    fn text<T: ElementIterator>(&self, node: T) -> String {
+        match self.text_type {
+            TextType::Own => node.own_text(),
+            TextType::All { join_str } => node.all_text(&join_str),
+            TextType::Attribute {
+                join_str,
+                attributes,
+            } => {
+                if let Some(join_str) = join_str {
+                    node.attrs_first_of(attributes.as_slice()).join(&join_str)
+                } else {
+                    node.attr_first_of(attributes.as_slice()).unwrap_or_default()
+                }
+            },
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -104,4 +142,70 @@ enum Matcher {
         if_true: Box<Matcher>,
         if_false: Option<Box<Matcher>>,
     },
+}
+
+impl Matcher {
+    pub fn exec(&self, node: Element) -> Box<dyn Any> {
+        match self {
+            Matcher::String { query } => {
+                let element = query.select(node);
+                query.text(element)
+            }
+            Matcher::Number { query } => todo!(),
+            Matcher::URL { query } => todo!(),
+            Matcher::Date {
+                query,
+                date_formats,
+            } => todo!(),
+            Matcher::Boolean {
+                query,
+                matches_regex,
+            } => todo!(),
+            Matcher::Array {
+                query,
+                split,
+                regex,
+            } => todo!(),
+            Matcher::Condition {
+                query,
+                matches_regex,
+                if_true,
+                if_false,
+            } => todo!(),
+        }
+        todo!()
+    }
+}
+
+#[cfg(test)]
+fn make_cfg() -> Matcher {
+    Matcher::Condition {
+        query: QueryMatcher {
+            selector: "h1.title".to_string(),
+            text_type: TextType::Own,
+            clean_with_regex_1: None,
+        },
+        matches_regex: "regex".to_string(),
+        if_true: Box::new(Matcher::String {
+            query: QueryMatcher {
+                selector: "h1.title".to_string(),
+                text_type: TextType::Own,
+                clean_with_regex_1: None,
+            },
+        }),
+        if_false: None,
+    }
+}
+
+#[test]
+fn test_case() -> Result<(), Box<dyn Error>> {
+    let doc = kuchiki::parse_html()
+        .from_utf8()
+        .from_file("tests/fragments/test.html")?;
+
+    let cfg = make_cfg();
+
+    cfg.exec(doc.select("html:first-of-type").unwrap());
+
+    Ok(())
 }
---
+++ src/util/mod.rs
@@ -0,0 +1,1 @@
+pub mod kuchiki_elements;
